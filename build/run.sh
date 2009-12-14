a5be7b6613ee22ae36169cb4f5d2ca72a52ab7d1
Lally Singh <lally@acm.org>
2009-12-13 22:17:21 -0500

    A Xephyr-based run script

---
+++ build/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if ! pgrep Xephyr 
+then
+  echo "Starting Xephyr"
+  pfexec Xephyr :1.0 &
+  xauth extract - $DISPLAY | DISPLAY=:1.0 xauth merge -
+  echo "You'll have to re-run this script to launch ioquake, it probably won't work this time."
+fi
+
+DISPLAY=:1.0 ./ioquake3.i386
+
