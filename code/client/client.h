3d1cdd97eeb42c0c7fda7acac960cf707a9b6e94
Lally Singh <lally@acm.org>
2009-12-27 02:56:21 -0500

    Analysis and ls_core have the obvious changes.  client.h was cleaned up for 80-column sanity (that's it).  find_src had its colors tweaked to distinguish between it and my command line.

--- code/client/client.h
+++ code/client/client.h
@@ -43,28 +43,40 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #define QKEY_FILE "qkey"
 #define QKEY_SIZE 2048
 
-#define	RETRANSMIT_TIMEOUT	3000	// time between connection packet retransmits
+#define	RETRANSMIT_TIMEOUT	3000	// time between connection packet
+									// retransmits
 
 // snapshots are a view of the server at a given time
 typedef struct {
 	qboolean		valid;			// cleared if delta parsing was invalid
 	int				snapFlags;		// rate delayed and dropped commands
 
-	int				serverTime;		// server time the message is valid for (in msec)
+	int				serverTime;		// server time the message is
+									// valid for (in msec)
 
 	int				messageNum;		// copied from netchan->incoming_sequence
 	int				deltaNum;		// messageNum the delta is from
-	int				ping;			// time from when cmdNum-1 was sent to time packet was reeceived
-	byte			areamask[MAX_MAP_AREA_BYTES];		// portalarea visibility bits
+	int				ping;			// time from when cmdNum-1 was
+									// sent to time packet was
+									// reeceived
+	byte			areamask[MAX_MAP_AREA_BYTES];		// portalarea
+														// visibility
+														// bits
 
 	int				cmdNum;			// the next cmdNum the server is expecting
-	playerState_t	ps;						// complete information about the current player at this time
+	playerState_t	ps;						// complete information
+											// about the current
+											// player at this time
 
-	int				numEntities;			// all of the entities that need to be presented
-	int				parseEntitiesNum;		// at the time of this snapshot
+	int				numEntities;			// all of the entities
+											// that need to be
+											// presented
+	int				parseEntitiesNum;		// at the time of this
+											// snapshot
 
-	int				serverCommandNum;		// execute all commands up to this before
-											// making the snapshot current
+	int				serverCommandNum;		// execute all commands up
+											// to this before making
+											// the snapshot current
 } clSnapshot_t;
 
 
@@ -84,36 +96,46 @@ typedef struct {
 	int		p_realtime;			// cls.realtime when packet was sent
 } outPacket_t;
 
-// the parseEntities array must be large enough to hold PACKET_BACKUP frames of
-// entities, so that when a delta compressed message arives from the server
-// it can be un-deltad from the original 
+// the parseEntities array must be large enough to hold PACKET_BACKUP
+// frames of entities, so that when a delta compressed message arives
+// from the server it can be un-deltad from the original
 #define	MAX_PARSE_ENTITIES	2048
 
 extern int g_console_field_width;
 
 typedef struct {
-	int			timeoutcount;		// it requres several frames in a timeout condition
-									// to disconnect, preventing debugging breaks from
-									// causing immediate disconnects on continue
+	int			timeoutcount;		// it requires several frames in a
+									// timeout condition to
+									// disconnect, preventing
+									// debugging breaks from causing
+									// immediate disconnects on
+									// continue
 	clSnapshot_t	snap;			// latest received from server
 
 	int			serverTime;			// may be paused during play
-	int			oldServerTime;		// to prevent time from flowing bakcwards
+	int			oldServerTime;		// to prevent time from flowing
+									// backwards
 	int			oldFrameServerTime;	// to check tournament restarts
-	int			serverTimeDelta;	// cl.serverTime = cls.realtime + cl.serverTimeDelta
-									// this value changes as net lag varies
-	qboolean	extrapolatedSnapshot;	// set if any cgame frame has been forced to extrapolate
-									// cleared when CL_AdjustTimeDelta looks at it
+	int			serverTimeDelta;	// cl.serverTime = cls.realtime +
+									// cl.serverTimeDelta this value
+									// changes as net lag varies
+	qboolean	extrapolatedSnapshot;	// set if any cgame frame has
+										// been forced to extrapolate
+										// cleared when
+										// CL_AdjustTimeDelta looks at
+										// it
 	qboolean	newSnapshots;		// set on parse of any valid packet
 
 	gameState_t	gameState;			// configstrings
 	char		mapname[MAX_QPATH];	// extracted from CS_SERVERINFO
 
-	int			parseEntitiesNum;	// index (not anded off) into cl_parse_entities[]
+	int			parseEntitiesNum;	// index (not anded off) into
+									// cl_parse_entities[]
 
 	int			mouseDx[2], mouseDy[2];	// added to by mouse events
 	int			mouseIndex;
-	int			joystickAxis[MAX_JOYSTICK_AXIS];	// set by joystick events
+	int			joystickAxis[MAX_JOYSTICK_AXIS];	// set by joystick
+													// events
 
 	// cgame communicates a few values to the client system
 	int			cgameUserCmdValue;	// current weapon to add to usercmd_t
@@ -121,25 +143,32 @@ typedef struct {
 
 	// cmds[cmdNumber] is the predicted command, [cmdNumber-1] is the last
 	// properly generated command
-	usercmd_t	cmds[CMD_BACKUP];	// each mesage will send several old cmds
-	int			cmdNumber;			// incremented each frame, because multiple
-									// frames may need to be packed into a single packet
+	usercmd_t	cmds[CMD_BACKUP];	// each mesage will send several
+									// old cmds
+	int			cmdNumber;			// incremented each frame, because
+									// multiple frames may need to be
+									// packed into a single packet
 
-	outPacket_t	outPackets[PACKET_BACKUP];	// information about each packet we have sent out
+	outPacket_t	outPackets[PACKET_BACKUP];	// information about each
+											// packet we have sent out
 
 	// the client maintains its own idea of view angles, which are
-	// sent to the server each frame.  It is cleared to 0 upon entering each level.
-	// the server sends a delta each frame which is added to the locally
-	// tracked view angles to account for standing on rotating objects,
-	// and teleport direction changes
+	// sent to the server each frame.  It is cleared to 0 upon
+	// entering each level.  the server sends a delta each frame which
+	// is added to the locally tracked view angles to account for
+	// standing on rotating objects, and teleport direction changes
 	vec3_t		viewangles;
 
-	int			serverId;			// included in each client message so the server
-												// can tell if it is for a prior map_restart
+	int			serverId;			// included in each client message
+									// so the server can tell if it is
+									// for a prior map_restart
 	// big stuff at end of structure so most offsets are 15 bits or less
 	clSnapshot_t	snapshots[PACKET_BACKUP];
 
-	entityState_t	entityBaselines[MAX_GENTITIES];	// for delta compression when not in previous frame
+	entityState_t	entityBaselines[MAX_GENTITIES];	// for delta
+													// compression
+													// when not in
+													// previous frame
 
 	entityState_t	parseEntities[MAX_PARSE_ENTITIES];
 } clientActive_t;
@@ -149,8 +178,9 @@ extern	clientActive_t		cl;
 /*
 =============================================================================
 
-the clientConnection_t structure is wiped when disconnecting from a server,
-either to go to a full screen console, play a demo, or connect to a different server
+The clientConnection_t structure is wiped when disconnecting from a
+server, either to go to a full screen console, play a demo, or connect
+to a different server
 
 A connection can be to either a server through the network layer or a
 demo through a file.
@@ -163,20 +193,26 @@ demo through a file.
 typedef struct {
 
 	int			clientNum;
-	int			lastPacketSentTime;			// for retransmits during connection
+	int			lastPacketSentTime;			// for retransmits during
+											// connection
 	int			lastPacketTime;				// for timeouts
 
 	netadr_t	serverAddress;
 	int			connectTime;				// for connection retransmits
 	int			connectPacketCount;			// for display on connection dialog
-	char		serverMessage[MAX_STRING_TOKENS];	// for display on connection dialog
+	char		serverMessage[MAX_STRING_TOKENS];	// for display on
+													// connection
+													// dialog
 
-	int			challenge;					// from the server to use for connecting
-	int			checksumFeed;				// from the server for checksum calculations
+	int			challenge;					// from the server to use
+											// for connecting
+	int			checksumFeed;				// from the server for
+											// checksum calculations
 
 	// these are our reliable messages that go to the server
 	int			reliableSequence;
-	int			reliableAcknowledge;		// the last one the server has executed
+	int			reliableAcknowledge;		// the last one the server
+											// has executed
 	char		reliableCommands[MAX_RELIABLE_COMMANDS][MAX_STRING_CHARS];
 
 	// server message (unreliable) and command (reliable) sequence
@@ -189,7 +225,10 @@ typedef struct {
 
 	// reliable messages received from server
 	int			serverCommandSequence;
-	int			lastExecutedServerCommand;		// last server command grabbed or executed with CL_GetServerCommand
+	int			lastExecutedServerCommand;		// last server command
+												// grabbed or executed
+												// with
+												// CL_GetServerCommand
 	char		serverCommands[MAX_RELIABLE_COMMANDS][MAX_STRING_CHARS];
 
 	// file transfer from server
@@ -210,25 +249,32 @@ typedef struct {
 	int			downloadBlock;	// block we are waiting for
 	int			downloadCount;	// how many bytes we got
 	int			downloadSize;	// how many bytes we got
-	char		downloadList[MAX_INFO_STRING]; // list of paks we need to download
-	qboolean	downloadRestart;	// if true, we need to do another FS_Restart because we downloaded a pak
+	char		downloadList[MAX_INFO_STRING]; // list of paks we need
+											   // to download
+	qboolean	downloadRestart;	// if true, we need to do another
+									// FS_Restart because we
+									// downloaded a pak
 
 	// demo information
 	char		demoName[MAX_QPATH];
 	qboolean	spDemoRecording;
 	qboolean	demorecording;
 	qboolean	demoplaying;
-	qboolean	demowaiting;	// don't record until a non-delta message is received
+	qboolean	demowaiting;	// don't record until a non-delta
+								// message is received
 	qboolean	firstDemoFrameSkipped;
 	fileHandle_t	demofile;
 
 	int			timeDemoFrames;		// counter of rendered frames
 	int			timeDemoStart;		// cls.realtime before first frame
-	int			timeDemoBaseTime;	// each frame will be at this time + frameNum * 50
+	int			timeDemoBaseTime;	// each frame will be at this time
+									// + frameNum * 50
 	int			timeDemoLastFrame;// time the last frame was rendered
 	int			timeDemoMinDuration;	// minimum frame duration
 	int			timeDemoMaxDuration;	// maximum frame duration
-	unsigned char	timeDemoDurations[ MAX_TIMEDEMO_DURATIONS ];	// log of frame durations
+
+    // log of frame durations
+	unsigned char	timeDemoDurations[ MAX_TIMEDEMO_DURATIONS ];	
 
 #ifdef USE_VOIP
 	qboolean speexInitialized;
@@ -301,9 +347,12 @@ typedef struct {
 typedef struct {
 	connstate_t	state;				// connection status
 
-	qboolean	cddialog;			// bring up the cd needed dialog next frame
+	qboolean	cddialog;			// bring up the cd needed dialog
+									// next frame
 
-	char		servername[MAX_OSPATH];		// name of server from original connect (used by reconnect)
+	char		servername[MAX_OSPATH];		// name of server from
+											// original connect (used
+											// by reconnect)
 
 	// when the server clears the hunk, all of these must be restarted
 	qboolean	rendererStarted;
@@ -316,7 +365,8 @@ typedef struct {
 	int			frametime;			// msec since last frame
 
 	int			realtime;			// ignores pause
-	int			realFrametime;		// ignoring pause, so console always works
+	int			realFrametime;		// ignoring pause, so console
+									// always works
 
 	int			numlocalservers;
 	serverInfo_t	localServers[MAX_OTHER_SERVERS];
@@ -457,7 +507,9 @@ void CL_InitRef( void );
 #ifndef STANDALONE
 qboolean CL_CDKeyValidate( const char *key, const char *checksum );
 #endif
-int CL_ServerStatus( char *serverAddress, char *serverStatusString, int maxLen );
+int CL_ServerStatus( char *serverAddress,
+					 char *serverStatusString,
+					 int maxLen );
 
 qboolean CL_CheckPaused(void);
 
@@ -467,7 +519,8 @@ qboolean CL_CheckPaused(void);
 typedef struct {
 	int			down[2];		// key nums holding it down
 	unsigned	downtime;		// msec timestamp
-	unsigned	msec;			// msec down this frame if both a down and up happened
+	unsigned	msec;			// msec down this frame if both a down
+								// and up happened
 	qboolean	active;			// current state
 	qboolean	wasPressed;		// set when down, not cleared when up
 } kbutton_t;
@@ -548,7 +601,9 @@ void	SCR_UpdateScreen (void);
 
 void	SCR_DebugGraph (float value, int color);
 
-int		SCR_GetBigStringWidth( const char *str );	// returns in virtual 640x480 coordinates
+int		SCR_GetBigStringWidth( const char *str );	// returns in
+													// virtual 640x480
+													// coordinates
 
 void	SCR_AdjustFrom640( float *x, float *y, float *w, float *h );
 void	SCR_FillRect( float x, float y, float width, float height, 
@@ -556,9 +611,17 @@ void	SCR_FillRect( float x, float y, float width, float height,
 void	SCR_DrawPic( float x, float y, float width, float height, qhandle_t hShader );
 void	SCR_DrawNamedPic( float x, float y, float width, float height, const char *picname );
 
-void	SCR_DrawBigString( int x, int y, const char *s, float alpha, qboolean noColorEscape );			// draws a string with embedded color control characters with fade
-void	SCR_DrawBigStringColor( int x, int y, const char *s, vec4_t color, qboolean noColorEscape );	// ignores embedded color control characters
-void	SCR_DrawSmallStringExt( int x, int y, const char *string, float *setColor, qboolean forceColor, qboolean noColorEscape );
+void	SCR_DrawBigString( int x, int y, const char *s,
+						   float alpha, qboolean noColorEscape );
+    // draws a string with embedded color control characters with fade
+
+void	SCR_DrawBigStringColor( int x, int y, const char *s,
+								vec4_t color, qboolean noColorEscape );
+    // ignores embedded color control characters
+
+void	SCR_DrawSmallStringExt( int x, int y, const char *string,
+								float *setColor, qboolean forceColor,
+								qboolean noColorEscape );
 void	SCR_DrawSmallChar( int x, int y, int ch );
 
 
@@ -570,7 +633,8 @@ void CL_PlayCinematic_f( void );
 void SCR_DrawCinematic (void);
 void SCR_RunCinematic (void);
 void SCR_StopCinematic (void);
-int CIN_PlayCinematic( const char *arg0, int xpos, int ypos, int width, int height, int bits);
+int CIN_PlayCinematic( const char *arg0, int xpos, int ypos,
+					   int width, int height, int bits);
 e_status CIN_StopCinematic(int handle);
 e_status CIN_RunCinematic (int handle);
 void CIN_DrawCinematic (int handle);
@@ -604,7 +668,9 @@ void LAN_SaveServersToCache( void );
 //
 // cl_net_chan.c
 //
-void CL_Netchan_Transmit( netchan_t *chan, msg_t* msg);	//int length, const byte *data );
+void CL_Netchan_Transmit( netchan_t *chan, msg_t* msg);	//int length,
+														//const byte
+														//*data );
 void CL_Netchan_TransmitNextFragment( netchan_t *chan );
 qboolean CL_Netchan_Process( netchan_t *chan, msg_t *msg );
 
--- code/ls/ls_core.c
+++ code/ls/ls_core.c
@@ -1,23 +1,32 @@
 #include "ls_core.h"
 #include <assert.h>
 
+/*
+  Quick convention:  New APIs are ls_, and ones designed to interact
+  with the existing codebase (e.g., called instead of existing
+  functions) are LS_.
+*/
 static qboolean s_enabled;
 static botlib_export_t *s_botlib;
+static clietnActive_t  *s_cl;
 
 typedef struct bot_debugpoly_s {
-  int inuse;
-  int color;
-  int numPoints;
-  vec3_t points[128];
+	int inuse;
+	int color;
+	int numPoints;
+	vec3_t points[128];
 } bot_debugpoly_t;
 
 static bot_debugpoly_t *s_debugpolygons;
 int s_bot_maxdebugpolys;
 
 qboolean LS_Enabled () {
-  return s_enabled && s_botlib;
+	return s_enabled && s_botlib;
 }
 
+void ls_set_pointers(clientActive_t *client_state) {
+	s_cl = client_state;
+};
 
 static QDECL ls_bi_print(int type, char *fmt, ...) {
 	char str[2048];
@@ -28,74 +37,102 @@ static QDECL ls_bi_print(int type, char *fmt, ...) {
 	va_end(ap);
 
 	switch(type) {
-		case PRT_MESSAGE: {
-			Com_Printf("%s", str);
-			break;
-		}
-		case PRT_WARNING: {
-			Com_Printf(S_COLOR_YELLOW "Warning: %s", str);
-			break;
-		}
-		case PRT_ERROR: {
-			Com_Printf(S_COLOR_RED "Error: %s", str);
-			break;
-		}
-		case PRT_FATAL: {
-			Com_Printf(S_COLOR_RED "Fatal: %s", str);
-			break;
-		}
-		case PRT_EXIT: {
-			Com_Error(ERR_DROP, S_COLOR_RED "Exit: %s", str);
-			break;
-		}
-		default: {
-			Com_Printf("unknown print type\n");
-			break;
-		}
+	case PRT_MESSAGE: {
+		Com_Printf("%s", str);
+		break;
+	}
+	case PRT_WARNING: {
+		Com_Printf(S_COLOR_YELLOW "Warning: %s", str);
+		break;
+	}
+	case PRT_ERROR: {
+		Com_Printf(S_COLOR_RED "Error: %s", str);
+		break;
+	}
+	case PRT_FATAL: {
+		Com_Printf(S_COLOR_RED "Fatal: %s", str);
+		break;
+	}
+	case PRT_EXIT: {
+		Com_Error(ERR_DROP, S_COLOR_RED "Exit: %s", str);
+		break;
+	}
+	default: {
+		Com_Printf("unknown print type\n");
+		break;
+	}
 	}
 }
 
 qboolean LS_Initialize(botlib_export_t **result) {
-  botlib_import_T botlib_import;
+	botlib_import_T botlib_import;
   
-  botlib_import.Print = ls_bi_print;
-  botlib_import.Trace = BotImport_Trace;
-  botlib_import.EntityTrace = BotImport_EntityTrace;
-  botlib_import.PointContents = BotImport_PointContents;
-  botlib_import.inPVS = BotImport_inPVS;
-  botlib_import.BSPEntityData = BotImport_BSPEntityData;
-  botlib_import.BSPModelMinsMaxsOrigin = BotImport_BSPModelMinsMaxsOrigin;
-  botlib_import.BotClientCommand = BotClientCommand;
+	botlib_import.Print = ls_bi_print;
+	botlib_import.Trace = BotImport_Trace;
+	botlib_import.EntityTrace = BotImport_EntityTrace;
+	botlib_import.PointContents = BotImport_PointContents;
+	botlib_import.inPVS = BotImport_inPVS;
+	botlib_import.BSPEntityData = BotImport_BSPEntityData;
+	botlib_import.BSPModelMinsMaxsOrigin = BotImport_BSPModelMinsMaxsOrigin;
+	botlib_import.BotClientCommand = BotClientCommand;
 
-  //memory management
-  botlib_import.GetMemory = BotImport_GetMemory;
-  botlib_import.FreeMemory = BotImport_FreeMemory;
-  botlib_import.AvailableMemory = Z_AvailableMemory;
-  botlib_import.HunkAlloc = BotImport_HunkAlloc;
+	//memory management
+	botlib_import.GetMemory = BotImport_GetMemory;
+	botlib_import.FreeMemory = BotImport_FreeMemory;
+	botlib_import.AvailableMemory = Z_AvailableMemory;
+	botlib_import.HunkAlloc = BotImport_HunkAlloc;
 
-  // file system access
-  botlib_import.FS_FOpenFile = FS_FOpenFileByMode;
-  botlib_import.FS_Read = FS_Read2;
-  botlib_import.FS_Write = FS_Write;
-  botlib_import.FS_FCloseFile = FS_FCloseFile;
-  botlib_import.FS_Seek = FS_Seek;
+	// file system access
+	botlib_import.FS_FOpenFile = FS_FOpenFileByMode;
+	botlib_import.FS_Read = FS_Read2;
+	botlib_import.FS_Write = FS_Write;
+	botlib_import.FS_FCloseFile = FS_FCloseFile;
+	botlib_import.FS_Seek = FS_Seek;
 
-  //debug lines
-  botlib_import.DebugLineCreate = BotImport_DebugLineCreate;
-  botlib_import.DebugLineDelete = BotImport_DebugLineDelete;
-  botlib_import.DebugLineShow = BotImport_DebugLineShow;
+	//debug lines
+	botlib_import.DebugLineCreate = BotImport_DebugLineCreate;
+	botlib_import.DebugLineDelete = BotImport_DebugLineDelete;
+	botlib_import.DebugLineShow = BotImport_DebugLineShow;
 
-  //debug polygons
-  botlib_import.DebugPolygonCreate = BotImport_DebugPolygonCreate;
-  botlib_import.DebugPolygonDelete = BotImport_DebugPolygonDelete;
+	//debug polygons
+	botlib_import.DebugPolygonCreate = BotImport_DebugPolygonCreate;
+	botlib_import.DebugPolygonDelete = BotImport_DebugPolygonDelete;
 
-  s_botlib = (botlib_export_t *)GetBotLibAPI( BOTLIB_API_VERSION, &botlib_import );
-  assert(s_botlib); 	// somehow we end up with a zero import.
-  if (s_botlib) {
-	*result = s_botlib;
-	return qtrue;
-  }
-  else {
-	return qfalse;
-  }
+	s_botlib = (botlib_export_t *)GetBotLibAPI( BOTLIB_API_VERSION, &botlib_import );
+	assert(s_botlib); 	// somehow we end up with a zero import.
+	if (s_botlib) {
+		*result = s_botlib;
+		return qtrue;
+	}
+	else {
+		return qfalse;
+	}
 }
+
+struct entity_value {
+	int entity_num;
+	vec3_t min, max;
+	double t;
+};
+
+void ls_trace(bsp_trace_t		*bsptrace,
+			  vec3_t			 start,
+			  vec3_t			 mins,
+			  vec3_t			 maxs,
+			  vec3_t			 end,
+			  int				 passent,
+			  int				 contentmask) {
+	// WARNING: this is some 3d-noob code.
+	
+	// figure out which planes to scan by the input vector.
+	bool x = end[0] - start[0] >= 0;
+	bool y = end[0] - start[1] >= 0;
+	bool z = end[0] - start[2] >= 0;
+
+	// scan s_cl for entities to trace.
+	entity_value   all[128];
+	for (int i=0; i<s_cl->parseEntitiesNum; ++i) {
+		
+	}
+}
+			  
